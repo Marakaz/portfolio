912abec38a340ad8a7b23adf7b7cb04249583eff
Patryk Michalski <marakazmic@gmail.com>
2020-10-09 19:02:12 +0200

    add .gitignore, add home.html , add path portfolio/templates/portfolio, edit irls.py, edit view.py, create media directory

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Project  ## import from our dictionary a Project class we created
 
-# Create your views here.
+def home(request):
+    projects = Project.objects.all()  # this grabs all the objects from the database that are project objects
+    return render(request, 'portfolio/home.html', {'projects': projects})
