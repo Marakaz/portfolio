f45587d8353b8400d7a84ad980ca8304aed6b50b
Patryk Michalski <marakazmic@gmail.com>
2020-11-04 15:47:18 +0100

    blog migration, modified blog models and templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Article
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html' )
+    articles = Article.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {'articles': articles})
